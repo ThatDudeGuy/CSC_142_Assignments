1e4f182d4fb85ba02d590059b10602f730fcbcb7
ThatDudeGuy <waffles1999@icloud.com>
2025-02-10 09:39:36 -0500

    Add: Week4 Lab7

---
+++ Week4/Lab7/task.cpp
@@ -0,0 +1,30 @@
+/*
+
+This program will make use of randomly generated integers. The way to use random
+    numbers in Java was shown in today's class, and is summarized here:
+    
+    - Detecting Odd and Even Numbers
+        Remember: any number 𝑛 is an even number if 𝑛 is evenly divisible by two. In other
+        words, integer division of 𝑛 by 2 will leave a remainder of 0 if 𝑛 is even, and 1 if 𝑛 is odd.
+        You can test for this by using the modulus operator, as shown in this pseudocode:
+        If ( (𝑛 % 2) is 0 ) :
+            𝑛 is even;
+        otherwise:
+            𝑛 is odd
+        For this lab you must create a separate method named isEven() as described here:
+            isEven():
+                This method takes one integer argument as input
+                This method returns the Boolean value true if the number is even,
+                otherwise it returns false.
+
+Your Task
+    Your program will consist of two methods: the main()
+    method, and the isEven() method described above.
+    The main() method will use a for loop to generate 1,000 random integers in the range
+    [0,100] inclusive. It should use the isEven() method to determine whether each random number
+    is even or odd.
+    When the loop is finished, the program should display, in a "creative" format, the number of
+    random numbers, even numbers, and odd numbers that were generated.
+    (your exact numbers will be different, and you may use your own specific wording):
+
+*/
